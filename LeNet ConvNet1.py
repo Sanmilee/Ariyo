b7a175d9666e7d2e8817d35c3dde247046f479fc
SanmiLee <sanmi4ariyo@gmail.com>
2019-02-12 23:43:40 +0800

    Changes

--- LeNet ConvNet1.py
+++ LeNet ConvNet1.py
@@ -1,18 +1,19 @@
 from __future__ import division, print_function, absolute_import
-
 import tensorflow as tf
-
 from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets("/temp/data", one_hot=True)
+
 
 learning_rate = 0.001
 num_steps = 2000
 batch_size = 128
 
+
 num_input = 784 # MNIST data input (img shape: 28*28)
 num_classes = 10 # MNIST total classes (0-9 digits)
 dropout = 0.25 # Dropout, probability to drop a unit
+
 
 def conv_net(x_dict, n_classes, dropout, reuse, is_training):
     # Define a scope for reusing the variables
@@ -26,12 +27,12 @@ def conv_net(x_dict, n_classes, dropout, reuse, is_training):
         x = tf.reshape(x_dict, shape=[-1, 28, 28, 1])
 
         # Convolution Layer with 32 filters and a kernel size of 5
-        conv1 = tf.layers.conv2d(x, 6, 5, activation=tf.nn.relu)
+        conv1 = tf.layers.conv2d(x, 32, 5, activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv1 = tf.layers.max_pooling2d(conv1, 2, 2)
 
         # Convolution Layer with 64 filters and a kernel size of 3
-        conv2 = tf.layers.conv2d(conv1, 16, 5, activation=tf.nn.relu)
+        conv2 = tf.layers.conv2d(conv1, 64, 5, activation=tf.nn.relu)
         # Max Pooling (down-sampling) with strides of 2 and kernel size of 2
         conv2 = tf.layers.max_pooling2d(conv2, 2, 2)
 
